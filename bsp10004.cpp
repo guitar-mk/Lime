b111594c73c19d83a187ba9596c9e3c72a2e75ff
guitar-mk <markus-kriener@web.de>
2026-02-17 10:31:28 +0100

    Add files via upload

---
+++ bsp10004.cpp
@@ -0,0 +1,10 @@
+//bsp10004.cpp
+#include <iostream>
+using namespace std;
+
+int main (){
+	char x=106,y='f';
+	cout << "\n" << x  << " " << y << " ";
+	cout << "\n" << (x+y) << "\n";
+	return 0;
+}	
