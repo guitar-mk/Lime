49552c39665694b40a5e1e75c2f32c6ea09e63b7
guitar-mk <markus-kriener@web.de>
2026-02-24 08:49:19 +0100

    Add files via upload

--- bsp10017.cpp
+++ bsp10017.cpp
@@ -3,16 +3,25 @@
 using namespace std;
 
 int main() {
+	// Variable wert wird erzeugt
 	double wert=0.0;
+	// Ausgabe auf dem Bildschirm
 	cout << "\nBitte eine Zahl eingeben: ";
-
+    // Tastatureingabe für Variable
 	cin >> wert;
+	// Abfrage von Wert=10?
 	if(wert == 10.0)
+	  // Ausgabe von Wert=10!
 	  cout << "Der Wert ist zehn. \n";
-	  else
-	    if(wert > 10.0)
+	// Abfrage Wert ist ungleich 10
+	else
+    // Abfrage Wert über 10?
+    if(wert > 10.0)
+	     // Ausgabe Wert größer 10 
 	      cout << "Der Wert ist groesser als zehn. \n";
-	    else
+	// Wert ist kleiner 10
+	else
+	      // Ausgabe Wert ist kleiner als 10
 	      cout << "Der Wert ist kleiner als zehn.  \n";
 	 return 0;
 }
