d95ddda727225d625a651d027e35507a2e742ffd
guitar-mk <markus-kriener@web.de>
2026-02-24 07:32:35 +0100

    Add files via upload

---
+++ bsp10013.cpp
@@ -0,0 +1,16 @@
+// bsp10013.cpp
+#include <iostream>
+using namespace std;
+int main(){
+	char wort[10];
+	wort[0]='B';
+	wort[1]='i';
+	wort[2]='l';
+	wort[3]='d';
+	wort[4]='-';
+	wort[5]=65;
+	wort[6]='\0';
+	
+	cout << wort << '\n';
+	return 0;
+} 
